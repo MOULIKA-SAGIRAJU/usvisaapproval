129d8c919e71ec95cc0bf9c942fc9d9a870f1684
MOULIKA-SAGIRAJU <nadimpallimoulika@gmail.com>
2024-02-21 22:44:46 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Moulika",
+    author_email="nadimpallimoulika@gmail.com",
+    packages=find_packages()
+)
